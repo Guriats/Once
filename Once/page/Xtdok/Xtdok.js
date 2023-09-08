775afdcb5ac048365c11c62016a1732e18cbaf85
UsuarioM <espacioguriats@gmail.com>
2023-09-08 13:36:25 +0200

    Once

--- Once/page/Xtdok/Xtdok.js
+++ Once/page/Xtdok/Xtdok.js
@@ -16,24 +16,41 @@ const template = () => {
 </section>
 
 
-<div id="cuadroInformacion">
 
+<div id="cuadroInformacion">
 <div id="InformacionTexto">
-    <h1>¿Qué es el Programa Por Talento Digital?</h1>
-    <p id="textoInformacion">Por Talento Digital es un programa de formación permanente en competencias digitales y profesiones tecnológicas de la Fundación ONCE orientado a la adquisición de conocimientos y cualificación tecnológica y digital de las personas con discapacidad para favorecer así su inclusión laboral en profesiones con alta demanda en el mercado de trabajo, multiplicando de esta forma sus perspectivas profesionales.</p>
+  <h1>¿Qué es el Programa Por Talento Digital?</h1>
+  <p id="textoInformacion">
+    Por Talento Digital es un programa de formación permanente en
+    competencias digitales y profesiones tecnológicas de la Fundación ONCE
+    orientado a la adquisición de conocimientos y cualificación
+    tecnológica y digital de las personas con discapacidad para favorecer
+    así su inclusión laboral en profesiones con alta demanda en el mercado
+    de trabajo, multiplicando de esta forma sus perspectivas
+    profesionales.
+  </p>
 </div>
 
 <div id="InformacionImagen">
-
-<div id="textoImagen">
-<h3> II Convocatoria de becas </h3>
-<p id="textoBanner">¡Ya se ha abierto la II Convocatoria de becas de formación individual. No pierdas la oportunidad!</p>
+  <div id="textoImagen">
+    <h3>II Convocatoria de becas</h3>
+    <p id="textoBanner">
+      ¡Ya se ha abierto la II Convocatoria de becas de formación
+      individual. No pierdas la oportunidad!
+    </p>
+  </div>
 </div>
 
 <div id="contenedorBanner">
-<img src="images/banner_home.png" alt="banner" id="banner"/>
+  <img src="images/banner_home.png" alt="banner" id="banner" />
 </div>
 </div>
+
+<section id="cursos">
+<div class="contenedorCursos"></div>
+
+<div class="contenedorCursos"></div>
+</section>
 
   `;
 };
